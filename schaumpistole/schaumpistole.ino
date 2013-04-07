a14bf3bbd3aa0d35c7ae2a5d7a1cda64fade30dd
Stefan Hechenberger <stefan@nortd.com>
2013-04-07 13:07:27 +0200

    arduino code for schaumpistole and servo module

--- schaumpistole/schaumpistole.ino
+++ schaumpistole/schaumpistole.ino
@@ -1,44 +1,78 @@
+// Released under the GNU General Public License, version 3.0 (GPLv3)
+// Copyright (c) 2013 REX|LAB, University of Innsbruck
+// Credit: Pedja Gavrilovic, Stefan Hechenberger, Roger Loho, Romedius Weiss
+
 
 #include <Servo.h> 
  
 Servo myservo;                
  
-//const int buttonPin = 5;     // the number of the pushbutton pin
-const int servoPin =  9;      // the number of the servo pin
-const int pottiPin = A0;
 
-// set debug to true if you want to monitor the angle on the serial output, set debug to false otherwise
-const boolean debug = false;
+const boolean DEBUG = false;
 
-const int ServoOpen = 5;
-const int ServoClosed = 107;
+const int panelButtonPin = 2;
+const int panelInPin = 3;
 
-// variables will change:
-int pottiState = 0;
-int angle= ServoClosed;
-   
-void setup() 
-{ 
-  
+const int panelNobPin = A0;
+const int remoteNobPin = A1;  // the one connected through "CONTROL"
+
+const int servoPin = 6;
+const int servoShut = 10;
+const int servoOpen = 100;
+
+int panelButtonState = 0;  
+int panelInState = 0;
+int panelNobVal = 0;
+int remoteNobVal = 0;
+
+int angle = 0;
+
+
+void setup() { 
+  // define input pin, use internal pullup resistor
+  // this means button needs to go from input pin to GND
+  pinMode(panelButtonPin, INPUT);
+  pinMode(panelInPin, INPUT);
+  digitalWrite(panelButtonPin, HIGH);  // enable pull-up resistor
+  digitalWrite(panelInPin, HIGH);  // enable pull-up resistor
+  // init servo
   myservo.attach(servoPin);  
-  pinMode(pottiPin, INPUT); 
-  
-  myservo.write(ServoOpen);
-  
-  if (debug){
+  myservo.write(servoShut);
+
+  if (DEBUG) {
     Serial.begin(9600);
-    while (!Serial) {
-      ; 
+  }
+}
+
+
+void loop() {
+  panelButtonState = !digitalRead(panelButtonPin);
+  panelInState = digitalRead(panelInPin);
+  panelNobVal = analogRead(panelNobPin);
+  remoteNobVal = analogRead(remoteNobPin);
+
+  if (panelButtonState == HIGH && panelInState == HIGH) {
+    // open serve to the higher value of the two nobs
+    if (panelNobVal > remoteNobVal) {
+      angle = map(1023-panelNobVal, 0, 1023, servoShut, servoOpen);
+    } else {
+      angle = map(1023-remoteNobVal, 0, 1023, servoShut, servoOpen);
     }
+
+    if (DEBUG) {
+      Serial.println(panelButtonState);
+      Serial.println(panelInState);
+      Serial.println(panelNobVal);
+      Serial.println(remoteNobVal);
+      Serial.println(angle);
+      Serial.println("---");
+      delay(600); 
+    }
+  } else {
+    angle = servoOpen;
   }
-}
-void loop() 
-{ 
-  pottiState= analogRead(pottiPin);
-  angle = map(pottiState, 0, 1023, ServoOpen, ServoClosed);
+
   myservo.write(angle);
-  if (debug){
-    Serial.println(angle);
-  }
-  delay(200);  
+  delay(50);
 }
+  
